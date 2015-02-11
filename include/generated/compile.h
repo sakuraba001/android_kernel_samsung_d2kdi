bab4509afb89c03743abc7595b44aa3e7f1c061e
sakuraba <sakura.bar.com@gmail.com>
2015-02-11 14:31:31 +0900

    scl21_ics_opensource

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Wed Oct 10 23:52:46 KST 2012"
+#define LINUX_COMPILE_BY "jiwooks"
+#define LINUX_COMPILE_HOST "jiwooks-desktop"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-67) "
